7c2b0b005e47499c854db810587f150519abc2be
Hayden <matiseeric@gmail.com>
2022-05-19 20:10:12 -0600

    Cleaned things up for release

--- ChangeUserspaceFont/ChangeUserSpaceFont.cs
+++ ChangeUserspaceFont/ChangeUserSpaceFont.cs
@@ -18,6 +18,7 @@ namespace ChangeUserspaceFont
         [AutoRegisterConfigKey]
         public static readonly ModConfigurationKey<bool> Enabled =
             new ModConfigurationKey<bool>("Enabled", "Enable Font Override", () => true);
+
         [AutoRegisterConfigKey]
         public static readonly ModConfigurationKey<Uri> FontURL =
             new ModConfigurationKey<Uri>("FontURL", "Font URL", () => new Uri("Neosdb:///c801b8d2522fb554678f17f4597158b1af3f9be3abd6ce35d5a3112a81e2bf39.ttf"));
@@ -33,10 +34,7 @@ namespace ChangeUserspaceFont
 
         [AutoRegisterConfigKey]
         public static readonly ModConfigurationKey<bool> TryHook =
-            new ModConfigurationKey<bool>("TryHook", "Try Hook", () => false);
-        [AutoRegisterConfigKey]
-        public static readonly ModConfigurationKey<bool> DebugValues =
-            new ModConfigurationKey<bool>("DebugValues", "Debug Values", () => false);
+            new ModConfigurationKey<bool>("TryHook", "Try Hook - Only check this if something breaks", () => false);
 
         private static ModConfiguration config;
     
@@ -54,24 +52,50 @@ namespace ChangeUserspaceFont
             private static StaticFont? fontAsset;
             private static bool fontHooked = false;
 
-            //public static bool Prefix()
-            //{
-            //    if (config == null) return false; 
-            //}
-
             [HarmonyPatch(typeof(Userspace), "OnAttach")]
             [HarmonyPostfix]
             public static void Postfix(Userspace __instance)
             {
-                if (config == null) return;
-
                 FontChain _fontChain = Userspace.UserspaceWorld.RootSlot.GetComponent<FontChain>();
 
-                Msg("FontChain in " + _fontChain.World.Name + " awoke on " + _fontChain.Slot.Name);
+                //Msg("FontChain in " + _fontChain.World.Name + " awoke on " + _fontChain.Slot.Name);
                 
                 if (config.GetValue(Enabled) && _fontChain.World == Userspace.UserspaceWorld && _fontChain.Slot == Userspace.UserspaceWorld.RootSlot)
                 {
                     HookFont(_fontChain);
+                }
+            }
+            public static void Postfix()
+            {
+                //Update any font properties on change
+                if (fontHooked && fontAsset.URL != config.GetValue(FontURL))
+                {
+                    fontAsset.URL.Value = config.GetValue(FontURL);
+                    Msg("Userspace font updated!");
+                }
+
+                if (fontHooked && fontAsset.Padding != config.GetValue(Padding))
+                {
+                    fontAsset.Padding.Value = config.GetValue(Padding);
+                    Msg("Userspace font padding updated!");
+                }
+
+                if (fontHooked && fontAsset.PixelRange != config.GetValue(PixelRange))
+                {
+                    fontAsset.PixelRange.Value = config.GetValue(PixelRange);
+                    Msg("Userspace font pixel range updated!");
+                }
+
+                if (fontHooked && fontAsset.GlyphEmSize != config.GetValue(GlyphEmSize))
+                {
+                    fontAsset.GlyphEmSize.Value = config.GetValue(GlyphEmSize);
+                    Msg("Userspace font glyph em size updated!");
+                }
+                // Manually retries hook if it failed before
+                if (config.GetValue(Enabled) && config.GetValue(TryHook))
+                {
+                    HookFont(Userspace.UserspaceWorld.RootSlot.GetComponent<FontChain>());
+                    config.Set(TryHook, false);
                 }
             }
 
@@ -98,46 +122,6 @@ namespace ChangeUserspaceFont
                     Msg(ex.ToString());
                     config.Set(TryHook, false);
                     return false;
-                }
-            }
-
-            public static void Postfix()
-            {
-                if (config == null) return;
-
-                if (config.GetValue(DebugValues))
-                {
-                    Msg(fontAsset, fontAsset.URL, fontAsset.Slot.Name, fontAsset.World.Name);
-                }
-
-                if (fontHooked && fontAsset.URL != config.GetValue(FontURL))
-                {
-                    fontAsset.URL.Value = config.GetValue(FontURL);
-                    Msg("Userspace font updated!");
-                }
-
-                if (fontHooked && fontAsset.Padding != config.GetValue(Padding))
-                {
-                    fontAsset.Padding.Value = config.GetValue(Padding);
-                    Msg("Userspace font padding updated!");
-                }
-
-                if (fontHooked && fontAsset.PixelRange != config.GetValue(PixelRange))
-                {
-                    fontAsset.PixelRange.Value = config.GetValue(PixelRange);
-                    Msg("Userspace font pixel range updated!");
-                }
-
-                if (fontHooked && fontAsset.GlyphEmSize != config.GetValue(GlyphEmSize))
-                {
-                    fontAsset.GlyphEmSize.Value = config.GetValue(GlyphEmSize);
-                    Msg("Userspace font glyph em size updated!");
-                }
-
-                if (config.GetValue(Enabled) && config.GetValue(TryHook))
-                {
-                    HookFont(Userspace.UserspaceWorld.RootSlot.GetComponent<FontChain>());
-                    config.Set(TryHook, false);
                 }
             }
 
